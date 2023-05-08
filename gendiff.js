1f3999809a748e942af6a37a33297b4987b34abf
pechaaa <pecheykinartem@gmail.com>
2023-05-08 14:06:37 +0500

    added version

--- gendiff.js
+++ gendiff.js
@@ -2,6 +2,7 @@ const { Command } = require("commander");
 const program = new Command();
 
 program
+   .version("0.1.0")
    .description("Compares two configuration files and shows a difference.")
    .option("-V, --version", "output the version number");
 
