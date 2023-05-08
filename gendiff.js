8373cf17f15e99f27871f5404a96fcd0e2ccbd95
pechaaa <pecheykinartem@gmail.com>
2023-05-08 12:32:45 +0500

    first stage of the  project

---
+++ gendiff.js
@@ -0,0 +1,8 @@
+const { Command } = require("commander");
+const program = new Command();
+
+program
+   .description("Compares two configuration files and shows a difference.")
+   .option("-V, --version", "output the version number");
+
+program.parse();
